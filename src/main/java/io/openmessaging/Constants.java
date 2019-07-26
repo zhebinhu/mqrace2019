44a36b94b3074930e6152712747b6010136b4cdf
huzhebin <huzhebin@meituan.com>
2019-07-26 16:23:01 +0800

    7.26 direct v1

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 
     //public final static String URL = "/users/huzhebin/data/";
 
-    public final static String URL = "/alidata1/race2019/data";
+    public final static String URL = "/alidata1/race2019/data/";
 
     public final static int MAPPED_SIZE = MESSAGE_SIZE * 32 * 1024 * 1024;
 }
