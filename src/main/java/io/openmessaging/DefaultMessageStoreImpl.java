ad5a7becea97d6d452257d5bceda3923312aff52
huzhebin <huzhebin@meituan.com>
2019-07-27 21:52:33 +0800

    Revert "7.27 direct v2 log5"
    This reverts commit b10d6ca7

--- src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
@@ -72,14 +72,14 @@ public class DefaultMessageStoreImpl extends MessageStore {
                 }
             }
         }
-        //System.out.println("1 " + aMin + " " + aMax + " " + tMin + " " + tMax + " " + (System.currentTimeMillis() - startTime));
+        System.out.println("1 " + aMin + " " + aMax + " " + tMin + " " + tMax + " " + (System.currentTimeMillis() - startTime));
         //        List<Message> result = forkJoinPool.invoke(new MergeTask(new ArrayList<>(queues.values()), 0, queues.size() - 1, aMin, aMax, tMin, tMax));
         //        semaphore.release();
         //        return result;
         if (!copied) {
             synchronized (this) {
                 if (!copied) {
-                    //copy();
+                    copy();
                     copied = true;
                 }
             }
@@ -90,7 +90,121 @@ public class DefaultMessageStoreImpl extends MessageStore {
 
     @Override
     public long getAvgValue(long aMin, long aMax, long tMin, long tMax) {
+        //        if (avgJoinPool == null) {
+        //            synchronized (this) {
+        //                if (avgJoinPool == null) {
+        //                    avgJoinPool = new ForkJoinPool(queues.size() * 2);
+        //                }
+        //            }
+        //        }
+        //        try {
+        //            return (long) avgJoinPool.submit(() -> queues.values().stream().parallel().map(queue -> queue.getMessage(aMin, aMax, tMin, tMax)).flatMap(Collection::stream).mapToLong(Message::getA).average().getAsDouble()).get().doubleValue();
+        //        } catch (InterruptedException e) {
+        //            e.printStackTrace();
+        //        } catch (ExecutionException e) {
+        //            e.printStackTrace();
+        //        }
+        //        //System.out.println("2 " + aMin + " " + aMax + " " + tMin + " " + tMax + " " + System.currentTimeMillis());
+        //        return 0L;
+        //return (long)queues.values().stream().parallel().map(queue -> queue.getMessage(aMin, aMax, tMin, tMax)).flatMap(Collection::stream).mapToLong(Message::getA).average().getAsDouble();
         return (int) reader.getMessage(aMin, aMax, tMin, tMax).stream().mapToLong(Message::getA).average().getAsDouble();
     }
+
+    private void copy() {
+        System.out.println("copy start at:" + System.currentTimeMillis());
+        PriorityQueue<Pair<Message, Queue>> priorityQueue = new PriorityQueue<>(new Comparator<Pair<Message, Queue>>() {
+            @Override
+            public int compare(Pair<Message, Queue> o1, Pair<Message, Queue> o2) {
+                int t = (int) (o1.fst.getT() - o2.fst.getT());
+                if (t == 0) {
+                    t = (int) (o1.fst.getA() - o2.fst.getA());
+                }
+                return t;
+            }
+        });
+        for (Queue queue : queues.values()) {
+            Message message = queue.copy();
+            Pair<Message, Queue> pair = new Pair<>(message, queue);
+            priorityQueue.add(pair);
+        }
+        while (!priorityQueue.isEmpty()) {
+            //System.out.println(count);
+            Pair<Message, Queue> pair = priorityQueue.poll();
+            reader.put(pair.fst);
+            Message newMessage = pair.snd.copy();
+            if (newMessage != null) {
+                Pair<Message, Queue> newPair = new Pair<>(newMessage, pair.snd);
+                priorityQueue.add(newPair);
+            }
+        }
+        queues.clear();
+        System.out.println("copy end at:" + System.currentTimeMillis());
+    }
+
+    //    public class MergeTask extends RecursiveTask<List<Message>> {
+    //        private int start;
+    //
+    //        private int end;
+    //
+    //        private List<Queue> queues;
+    //
+    //        private long aMin;
+    //
+    //        private long aMax;
+    //
+    //        private long tMin;
+    //
+    //        private long tMax;
+    //
+    //        MergeTask(List<Queue> queues, int start, int end, long aMin, long aMax, long tMin, long tMax) {
+    //            this.queues = queues;
+    //            this.start = start;
+    //            this.end = end;
+    //            this.aMin = aMin;
+    //            this.aMax = aMax;
+    //            this.tMin = tMin;
+    //            this.tMax = tMax;
+    //        }
+    //
+    //        protected List<Message> compute() {
+    //            List<Message> result;
+    //            if (start == end) {
+    //                return queues.get(start).getMessage(aMin, aMax, tMin, tMax);
+    //            } else {
+    //                int mid = (start + end) / 2;
+    //                MergeTask leftTask = new MergeTask(queues, start, mid, aMin, aMax, tMin, tMax);
+    //                MergeTask rightTask = new MergeTask(queues, mid + 1, end, aMin, aMax, tMin, tMax);
+    //
+    //                leftTask.fork();
+    //                rightTask.fork();
+    //
+    //                List<Message> leftResult = leftTask.join();
+    //                List<Message> rightResult = rightTask.join();
+    //
+    //                result = merge(leftResult, rightResult);
+    //            }
+    //            return result;
+    //        }
+    //
+    //        private List<Message> merge(List<Message> a, List<Message> b) {
+    //            List<Message> result = new ArrayList<>();
+    //            int i = 0;
+    //            int j = 0;
+    //            while (i < a.size() && j < b.size()) {
+    //                if (a.get(i).getT() <= b.get(j).getT()) {
+    //                    result.add(a.get(i++));
+    //                } else {
+    //                    result.add(b.get(j++));
+    //                }
+    //            }
+    //            while (i < a.size()) {
+    //                result.add(a.get(i++));
+    //            }
+    //            while (j < b.size()) {
+    //                result.add(b.get(j++));
+    //            }
+    //            return result;
+    //        }
+    //    }
 
 }
