06b584bcb81c21b0c68e8a3db74494572f3a09f9
huzhebin <huzhebin@meituan.com>
2019-07-27 21:53:25 +0800

    Revert "7.26 direct v1"
    This reverts commit 44a36b94

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -6,15 +6,11 @@ package io.openmessaging;
 public class Constants {
     public final static int MESSAGE_SIZE = 50;
 
-    public final static int MESSAGE_NUM = 4*1024;
+    public final static int MESSAGE_NUM = 1024;
 
-    public final static String URL = "/users/huzhebin/data/";
+    //public final static String URL = "/users/huzhebin/data/";
 
-    //public final static String URL = "/alidata1/race2019/data/";
+    public final static String URL = "/alidata1/race2019/data";
 
     public final static int MAPPED_SIZE = MESSAGE_SIZE * 32 * 1024 * 1024;
-
-    public final static int INDEX_RATE = 30;
-
-    public final static int READ_PARALLEL = 4;
 }
