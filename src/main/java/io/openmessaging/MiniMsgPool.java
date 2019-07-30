c5622efb3e9a29559fa2a6f31252446fbcf40155
huzhebin <huzhebin@meituan.com>
2019-07-30 15:51:24 +0800

    合并分支

--- src/main/java/io/openmessaging/MiniMsgPool.java
+++ src/main/java/io/openmessaging/MiniMsgPool.java
@@ -4,18 +4,18 @@ package io.openmessaging;
  * Created by huzhebin on 2019/07/30.
  */
 public class MiniMsgPool {
-    MiniMsg[] miniMsgs = new MiniMsg[1000000];
+    MiniMsg[] miniMsgs = new MiniMsg[500000];
 
     volatile int index = 0;
 
     MiniMsgPool() {
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 500000; i++) {
             miniMsgs[i] = new MiniMsg();
         }
     }
 
     public synchronized MiniMsg get() {
-        index = (index + 1) % 1000000;
+        index = (index + 1) % 500000;
         return miniMsgs[index];
     }
 }
