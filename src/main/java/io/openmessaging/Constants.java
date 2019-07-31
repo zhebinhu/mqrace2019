e9e6fbc3a2d6d741f754d734d844e3be6802158e
huzhebin <huzhebin@meituan.com>
2019-07-31 09:13:27 +0800

    调整求平均逻辑

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -4,24 +4,19 @@ package io.openmessaging;
  * Created by huzhebin on 2019/07/23.
  */
 public class Constants {
-    // T 的大小
+    // T 和 A 的大小
     public final static int MESSAGE_SIZE = 16;
 
     public final static int MESSAGE_NUM = 2 * 1024;
-
-    // A 的大小
-    public final static int VALUE_SIZE = 8;
-
-    public final static int VALUE_NUM = 2 * 1024;
 
     // data 的大小
     public final static int DATA_SIZE = 34;
 
     public final static int DATA_NUM = 2 * 1024;
 
-    //public final static String URL = "/users/huzhebin/data/";
+    public final static String URL = "/users/huzhebin/data/";
 
-    public final static String URL = "/alidata1/race2019/data/";
+    //public final static String URL = "/alidata1/race2019/data/";
 
     public final static int INDEX_RATE = 256;
 }
