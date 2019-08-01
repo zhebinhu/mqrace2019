3626929d0cc58a52c5abd2deb63c90dd93a2f6ad
huzhebin <huzhebin@meituan.com>
2019-08-01 14:28:33 +0800

    内存索引v1

--- src/test/java/io/openmessaging/DemoTester.java
+++ src/test/java/io/openmessaging/DemoTester.java
@@ -14,7 +14,7 @@ public class DemoTester {
     public static void main(String args[]) throws Exception {
         //评测相关配置
         //发送阶段的发送数量，也即发送阶段必须要在规定时间内把这些消息发送完毕方可
-        int msgNum  = 1200000000;
+        int msgNum  = 200000000;
         //发送阶段的最大持续时间，也即在该时间内，如果消息依然没有发送完毕，则退出评测
         int sendTime = 10 * 60 * 1000;
         //查询阶段的最大持续时间，也即在该时间内，如果消息依然没有消费完毕，则退出评测
@@ -60,38 +60,38 @@ public class DemoTester {
         long maxCheckTime = System.currentTimeMillis() + checkTime;
 
         //Step2: 查询聚合消息
-//        long msgCheckStart = System.currentTimeMillis();
-//        AtomicLong msgCheckTimes = new AtomicLong(0);
-//        AtomicLong msgCheckNum = new AtomicLong(0);
-//        Thread[] msgChecks = new Thread[checkTsNum];
-//        for (int i = 0; i < checkTsNum; i++) {
-//            msgChecks[i] = new Thread(new MessageChecker(messageStore, maxCheckTime, checkTimes, msgNum, maxMsgCheckSize, msgCheckTimes, msgCheckNum));
-//        }
-//        for (int i = 0; i < checkTsNum; i++) {
-//            msgChecks[i].start();
-//        }
-//        for (int i = 0; i < checkTsNum; i++) {
-//            msgChecks[i].join();
-//        }
-//        long msgCheckEnd = System.currentTimeMillis();
-//        System.out.printf("Message Check: %d ms Num:%d\n", msgCheckEnd - msgCheckStart, msgCheckNum.get());
+        long msgCheckStart = System.currentTimeMillis();
+        AtomicLong msgCheckTimes = new AtomicLong(0);
+        AtomicLong msgCheckNum = new AtomicLong(0);
+        Thread[] msgChecks = new Thread[checkTsNum];
+        for (int i = 0; i < checkTsNum; i++) {
+            msgChecks[i] = new Thread(new MessageChecker(messageStore, maxCheckTime, checkTimes, msgNum, maxMsgCheckSize, msgCheckTimes, msgCheckNum));
+        }
+        for (int i = 0; i < checkTsNum; i++) {
+            msgChecks[i].start();
+        }
+        for (int i = 0; i < checkTsNum; i++) {
+            msgChecks[i].join();
+        }
+        long msgCheckEnd = System.currentTimeMillis();
+        System.out.printf("Message Check: %d ms Num:%d\n", msgCheckEnd - msgCheckStart, msgCheckNum.get());
 
         //Step3: 查询聚合结果
         long checkStart = System.currentTimeMillis();
-        AtomicLong valueCheckTimes = new AtomicLong(0);
-        AtomicLong valueCheckNum = new AtomicLong(0);
-        Thread[] checks = new Thread[checkTsNum];
-        for (int i = 0; i < checkTsNum; i++) {
-            checks[i] = new Thread(new ValueChecker(messageStore, maxCheckTime, checkTimes, msgNum, maxValueCheckSize, valueCheckTimes, valueCheckNum));
-        }
-        for (int i = 0; i < checkTsNum; i++) {
-            checks[i].start();
-        }
-        for (int i = 0; i < checkTsNum; i++) {
-            checks[i].join();
-        }
-        long checkEnd = System.currentTimeMillis();
-        System.out.printf("Value Check: %d ms Num: %d\n", checkEnd - checkStart, valueCheckNum.get());
+//        AtomicLong valueCheckTimes = new AtomicLong(0);
+//        AtomicLong valueCheckNum = new AtomicLong(0);
+//        Thread[] checks = new Thread[checkTsNum];
+//        for (int i = 0; i < checkTsNum; i++) {
+//            checks[i] = new Thread(new ValueChecker(messageStore, maxCheckTime, checkTimes, msgNum, maxValueCheckSize, valueCheckTimes, valueCheckNum));
+//        }
+//        for (int i = 0; i < checkTsNum; i++) {
+//            checks[i].start();
+//        }
+//        for (int i = 0; i < checkTsNum; i++) {
+//            checks[i].join();
+//        }
+//        long checkEnd = System.currentTimeMillis();
+//        System.out.printf("Value Check: %d ms Num: %d\n", checkEnd - checkStart, valueCheckNum.get());
 //
 //        //评测结果
 //        System.out.printf("Total Score:%d\n", (msgNum / (sendSend- sendStart) + msgCheckNum.get() / (msgCheckEnd - msgCheckStart) + valueCheckNum.get() / (msgCheckEnd - msgCheckStart)));
