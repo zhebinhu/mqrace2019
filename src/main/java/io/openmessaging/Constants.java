d4f9a40b116acecab22177910b5e24a49c927ebc
15528166872 <344942688@qq.com>
2019-08-02 22:30:37 +0800

    调整数值

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
 
     public final static int DATA_NUM = 4 * 1024;
 
-    public final static String URL = "/users/huzhebin/data/";
+    //public final static String URL = "/users/huzhebin/data/";
 
-    //public final static String URL = "/alidata1/race2019/data/";
+    public final static String URL = "/alidata1/race2019/data/";
 }
