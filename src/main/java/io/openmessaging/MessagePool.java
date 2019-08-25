31108b733f1ba12bcb084c7de052fd5158258071
huzhebin <huzhebin@meituan.com>
2019-08-25 12:32:12 +0800

    baseline

--- src/main/java/io/openmessaging/MessagePool.java
+++ src/main/java/io/openmessaging/MessagePool.java
@@ -4,18 +4,18 @@ package io.openmessaging;
  * Created by huzhebin on 2019/07/28.
  */
 public class MessagePool {
-    Message[] messages = new Message[100000];
+    Message[] messages = new Message[120000];
 
     volatile int index = 0;
 
     MessagePool() {
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 120000; i++) {
             messages[i] = new Message(0, 0, new byte[34]);
         }
     }
 
     public synchronized Message get() {
-        index = (index + 1) % 100000;
+        index = (index + 1) % 120000;
         return messages[index];
     }
 }
