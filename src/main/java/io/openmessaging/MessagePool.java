65067163e44fe66784c9b26677685a11d0d84087
huzhebin <huzhebin@meituan.com>
2019-08-25 10:08:33 +0800

    getMessage优化

--- src/main/java/io/openmessaging/MessagePool.java
+++ src/main/java/io/openmessaging/MessagePool.java
@@ -4,18 +4,18 @@ package io.openmessaging;
  * Created by huzhebin on 2019/07/28.
  */
 public class MessagePool {
-    Message[] messages = new Message[100000];
+    Message[] messages = new Message[160000];
 
     volatile int index = 0;
 
     MessagePool() {
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 160000; i++) {
             messages[i] = new Message(0, 0, new byte[34]);
         }
     }
 
     public synchronized Message get() {
-        index = (index + 1) % 100000;
+        index = (index + 1) % 160000;
         return messages[index];
     }
 }
--- src/main/java/io/openmessaging/Reader/TimeReader.java
+++ src/main/java/io/openmessaging/Reader/TimeReader.java
@@ -5,8 +5,6 @@ import io.openmessaging.HalfByte;
 import io.openmessaging.Message;
 import io.openmessaging.TimeTags;
 
-import java.nio.ByteBuffer;
-
 /**
  * Created by huzhebin on 2019/08/07.
  */
@@ -20,13 +18,15 @@ public class TimeReader {
 
     private HalfByte halfByte = new HalfByte((byte) 0);
 
-    private volatile boolean init = false;
-
     private long tag = 0;
+
+    private long max = 0;
 
     public void put(Message message) {
         long t = message.getT();
-
+        if (t > max) {
+            max = t;
+        }
         if (tag == 0 || t > tag + 15) {
             tag = t;
             timeTags.add(t, msgNum);
@@ -46,8 +46,7 @@ public class TimeReader {
         //cache.put(msgNum / 2, halfByte.getByte());
         cache[msgNum / 2] = halfByte.getByte();
         //System.out.println("time max:" + max + " count256:" + count256 + " count65536:" + count65536 + " count15:" + count15);
-        System.out.println("msgnum:" + msgNum);
-        init = true;
+        System.out.println("max:" + max);
     }
 
     public int getOffset(long time) {
