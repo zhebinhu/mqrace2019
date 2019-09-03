1dbf9b63bb310bb7234f20fde03588856443be4c
zhebinhu <huzhebin2017@gmail.com>
2019-09-03 15:45:13 +0800

    value分片 test

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -22,7 +22,7 @@ public class Constants {
     public final static int VALUE_SIZE = 8;
 
     // value 切片
-    public final static int VALUE_BLOCKS = 7;
+    public final static int VALUE_BLOCKS = 9;
 
     // 初始流量
     public final static int INITIAL_FLOW = 10000000;
